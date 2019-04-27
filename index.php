0a2daa1b3e525634fc73dc3983157aa2368f9be8
robelsec <robelhossainsec@gmail.com>
2019-04-27 11:09:22 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="fonts/css/all.css" >
+        <!--<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">-->
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a class="active nav-link">Default Theme</a></li>
+                    <!--<li class="nav-item"><a class="nav-link">Blue Theme</a></li>-->
+                    <li  class="nav-item"><a class="nav-link" href="">Green Theme</a></li>
+                    <li class="nav-item"><a  class="nav-link" href="">Orange Theme</a></li>
+                    <li class="nav-item"><a href=""  class="nav-link">Red Theme</a></li>
+                    <li class="nav-item"><a href=""  class="nav-link">Yellow Theme</a></li>
+
+                </ul>
+
+
+                </ul> 
+
+            </div>
+
+            <section class="section-wrapper ">
+
+                <div class="main-title">
+                    <h2 class="text-center">MIXED THEME( 4 Column )</h2>    
+                </div>                            
+                <div class="row">
+                    <!--start 1st plan-->
+                    <div class="col-md-3">
+
+                        <div class="square1">
+                            <div class="package">
+                                <!--<img src="img/arrow.png" class="arrow">-->
+                                <img src="img/line.png" class="line">
+                                <div class="title">BABY PLAN</div>
+                                <span class="amount">$99<sup>.99</sup></span>
+                                <div class="duration">PER MONTH</div>
+                                <span class="package_icon"><i class="far fa-bell fa-2x"></i></span>
+
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder1">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>10GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>1GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>100 databases</button>
+                                <button type="button" class="list-group-item"><i class="far fa-envelope"></i>500 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>5GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>24/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-danger list-grou-item"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+                    <!--start 2nd plan-->
+
+                    <div class="col-md-3">
+
+                        <div class="square2">
+                            <div class="package">
+                                <img src="img/line.png">
+                                <div class="title">HATCHLING PLAN </div>
+                                <!--                                <div class="ribbon-badge">
+                                                                    <span class="amount">9%</span>
+                                                                    <span class="badge-text">OFF</span>    
+                                                                </div>-->
+                                <div class="package_amount_duration">
+                                    <span class="amount">$199<sup>.99</sup></span>
+                                    <div class="duration">PER MONTH</div>
+                                    <span class="package_icon"><i class="fas fa-ticket-alt fa-2x"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder2">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>20GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>2GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>100 databases</button>
+                                <button type="button" class="list-group-item"><i class="far fa-envelope"></i>500 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>3GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>24/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-primary"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+
+                    <!--start 3red plan-->
+
+                    <div class="col-md-3">
+
+                        <div class="square3">
+                            <div class="package">
+                                <img src="img/line.png" >
+                                <div class="title">BUISNESS PLAN</div>
+                                <span class="amount">$377<sup>.99</sup></span>
+                                <div class="duration">PER MONTH</div>
+                                <span class="package_icon"><i class="far fa-clock fa-2x"></i></span>
+
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder3">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>50GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>10GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>300 databases</button>
+                                <button type="button" class="list-group-item"><i class="far fa-envelope"></i>50 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>5GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>50/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-success list-grou-item"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+                    <!--start 4th plan-->
+
+                    <div class="col-md-3">
+
+                        <div class="square4">
+                            <div class="package">
+                                <img src="img/line.png">
+                                <div class="title">Enterprise Plan</div>
+                                <span class="amount">$733<sup>.99</sup></span>
+                                <div class="duration">PER MONTH</div>
+                                <span class="package_icon"><i class="fas fa-briefcase fa-2x"></i></span>
+
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder4">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>10GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>1GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>100 databases</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-envelope"></i>500 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>5GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>24/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-info list-grou-item"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <!--2nd row-->
+                <div class="main-title">
+                    <h2 class="text-center">MIXED THEME( 3 Column )</h2>    
+                </div> 
+                <div class="row">
+                    <!--start 1st plan-->
+                    <div class="col-md-4">
+
+                        <div class="square1">
+                            <div class="package">
+                                <!--<img src="img/arrow.png" class="arrow">-->
+                                <img src="img/line.png" class="line">
+                                <div class="title">BABY PLAN</div>
+                                <span class="amount">$99<sup>.99</sup></span>
+                                <div class="duration">PER MONTH</div>
+                                <span class="package_icon"><i class="far fa-bell fa-2x"></i></span>
+
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder1">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>10GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>1GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>100 databases</button>
+                                <button type="button" class="list-group-item"><i class="far fa-envelope"></i>500 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>5GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>24/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-danger list-grou-item"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+                    <!--start 2nd plan-->
+
+                    <div class="col-md-4">
+
+                        <div class="square2">
+                            <div class="package">
+                                <img src="img/line.png">
+                                <div class="title">HATCHLING PLAN </div>
+                                <!--                                <div class="ribbon-badge">
+                                                                    <span class="amount">9%</span>
+                                                                    <span class="badge-text">OFF</span>    
+                                                                </div>-->
+                                <div class="package_amount_duration">
+                                    <span class="amount">$199<sup>.99</sup></span>
+                                    <div class="duration">PER MONTH</div>
+                                    <span class="package_icon"><i class="fas fa-ticket-alt fa-2x"></i></span>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder2">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>20GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>2GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>100 databases</button>
+                                <button type="button" class="list-group-item"><i class="far fa-envelope"></i>500 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>3GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>24/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-primary"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+
+                    <!--start 3red plan-->
+
+                    <div class="col-md-4">
+
+                        <div class="square3">
+                            <div class="package">
+                                <img src="img/line.png" >
+                                <div class="title">BUISNESS PLAN</div>
+                                <span class="amount">$377<sup>.99</sup></span>
+                                <div class="duration">PER MONTH</div>
+                                <span class="package_icon"><i class="far fa-clock fa-2x"></i></span>
+
+                            </div>
+                        </div>
+
+
+                        <div class="placeholder3">
+                            <div class="list-group">
+
+                                <button type="button" class="list-group-item"><i class="fas fa-desktop"></i>50GB Disk Space</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-tasks"></i>10GB Ram</button>
+                                <button type="button" class="list-group-item"><i class="fas fa-database"></i>300 databases</button>
+                                <button type="button" class="list-group-item"><i class="far fa-envelope"></i>50 Emails</button>
+                                <button type="button" class="list-group-item"><i class="far fa-heart"></i>5GB Bandwidth</button>
+                                <button type=button" class="list-group-item"><i class="fas fa-check-double"></i>99.99% Uptime Guaranteed</button>
+                                <button type="button" class="list-group-item"><i class="fa fa-wrench"></i>50/7 Top Quality Support</button>
+
+                            </div>
+                        </div>
+                        <div class="selectpackage">
+                            <button class="btn-success list-grou-item"><i class="fa fa-shopping-cart">ORDER NOW</i></button>
+                        </div>
+                    </div>
+
+                    </div>
+
+
+                </div>
+
+               
+
+
+            </section>
+        </div>
+
+
+
+    </body>
+</html>
